c6b3236b0ffe41bf11cff0ebab70ff7bd2097d56
Darya Kaviani <daryakavi@gmail.com>
2022-09-19 01:08:22 -0700

    Add liquidations

--- bullSim.py
+++ bullSim.py
@@ -8,6 +8,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from pathlib import Path
+from operator import add
 # 
 
 # %% Params
@@ -16,9 +17,19 @@ from pathlib import Path
 S0 = 100
 mu = 0
 default_sigma = 0.8/np.sqrt(365) # Specify vol per period
+historical_sigma = 0.86807856209
+squeeth_rv = 0.8881961241
+eth_rv = 0.9149563106
+uni_iv = 0.8680785622
+funding = 0.08078018468
+squeeth_iv = 1.046845346
+tau = 17.5/365
+
 T = 365
 # Uni Fee Yield
 uniFeeYield = 0.0002580686267
+# Constant slippage
+slippage_cost = 0.01
 minCR = 1.75
 maxCR = 3
 
@@ -63,19 +74,26 @@ iterateGbmTest()
 lmDictionary = [(0.7,1), (0.6,1), (0.5,1), (0.618034,0.689808), (0.4,1), (0.3,1), (0.618034,0.48541)]
 
 # %% Simulator
-def simBullLite(sigma, l, m):
+def simBullLite(sigma, l, m, d = 1):
+    # d is days between hedge
     #sigma = 0 # zero vol checks
     S = liteGBM(S0=S0,mu=mu,sigma=sigma,T=T)
-    f = sigma**2 # Funding
+    f = d*((squeeth_iv/np.sqrt(T))**2) # Funding
 
     # l: % to LP: 0.2
     # m: oSQTH LP to mint ratio: 3
     # r: ETH return -> ln(S1) - ln(S0) -> what continuously compounded rate would get me from 100 to 105
     # f: funding -> sigma^2
 
-    r = S[1:]/S[0:-1] - 1
-    returns = (1+r)*((1-l)/l + 2*np.sqrt(1+r)*np.sqrt(1-f)+m -(1+m)*(1+r)*(1-f))/((1-l)/l+1) - 1 + uniFeeYield
-    return returns
+    r = S[d:]/S[0:-d] - 1
+    bull_returns = (1+r)*((1-l)/l + 2*np.sqrt(1+r)*np.sqrt(1-f)*(1+(d*uniFeeYield))+m -(1+m)*(1+r)*(1-f))/((1-l)/l+1) - 1
+    trade_size = l*(-1*np.sqrt((1-f)*(1+r)) - m*(bull_returns+1) + (1+m)*(1-f)*(1+r))
+    net_returns = bull_returns - np.abs(trade_size * slippage_cost)
+    CRs = [getCR(l, m, historical_sigma)]
+    for i in range(1, len(r)):
+        next_CR = (((1-l)/l + np.sqrt(1-f)*np.sqrt(1+r[i]) + m + np.sqrt(1-f)*np.sqrt(1+r[i])/np.exp(squeeth_iv**2 *tau))/ (1/l +m +1/np.exp(squeeth_iv**2 * tau))) * CRs[i - 1]
+        CRs.append(next_CR)
+    return net_returns, CRs
 
 # %%
 def simBullLiteSigmas(sigmas):
@@ -104,7 +122,7 @@ def getCR(l, m, vol, ethDeposited=100):
 # l = ethInVault/ethInLP
 # m = oSQTHToSell/oSQTHInLP
 
-def simBullLiteSigmasAverage(count, sigmas):
+def simBullLiteSigmasAverage(count):
     lm = generateLM()
     results = pd.DataFrame({'sigma':np.arange(0.4, 1.3, 0.1)})
     for i, row in lm.iterrows():
@@ -121,7 +139,6 @@ def simBullLiteSigmasAverage(count, sigmas):
                 for i in range(count):
                     bullReturnSum += sum(simBullLite(sigma/np.sqrt(T), l, m))
                 row[(l, m)] = bullReturnSum/count
-    historical_sigma = 0.8979
     slopes, y_intercepts, ls, ms, deltas, historical = [], [], [], [], [], []
     for (colname, values) in results.iteritems():
         if colname != 'sigma':
@@ -151,17 +168,113 @@ def simBullLiteSigmasAverage(count, sigmas):
     filepath.parent.mkdir(parents=True, exist_ok=True)
     trendlines.to_csv(filepath) 
 
+def simBullLiteSigmasTrue(count):
+    results = pd.DataFrame({'sigma':np.arange(0.4, 1.3, 0.1)})
+    results[(1, 0)] = np.nan
+
+    for i, row in results.iterrows():
+        sigma = results['sigma'][i]
+        l, m = 1, 0
+        bullReturnSum = 0
+        for i in range(count):
+            bullReturnSum += sum(simBullLite(sigma/np.sqrt(T), l, m))
+        row[(l, m)] = bullReturnSum/count
+    slopes, y_intercepts, ls, ms, deltas, historical = [], [], [], [], [], []
+    for (colname, values) in results.iteritems():
+        if colname != 'sigma':
+            l, m = colname
+            a, b = np.polyfit(results['sigma'], values, 1)
+            slopes.append(a)
+            y_intercepts.append(b)
+            ls.append(l)
+            ms.append(m)
+            deltas.append(getDelta(l, m))
+            historical.append((a*historical_sigma) + b)
+    x_intercepts = -1*np.divide(y_intercepts,slopes)
+    trendlines = pd.DataFrame({'slopes':slopes,
+                               'y_intercepts':y_intercepts,
+                               'x_intercepts':x_intercepts,
+                                'l': ls,
+                                'm': ms,
+                                'delta': deltas,
+                                'historical': historical
+                              })
+
+    filepath = Path('/Users/daryakaviani/bullSim/results.csv') 
+    filepath.parent.mkdir(parents=True, exist_ok=True)
+    results.to_csv(filepath) 
+
+    filepath = Path('/Users/daryakaviani/bullSim/trendline.csv') 
+    filepath.parent.mkdir(parents=True, exist_ok=True)
+    trendlines.to_csv(filepath) 
+
+def simBullLiteSigmasTrueHistorical(count):
+    l, m = 1, 0
+    results = pd.DataFrame()
+    results[(l, m)] = np.nan
+
+    returns = []
+    bullReturnSum = 0
+    for i in range(count):
+        bullReturns = simBullLite(eth_rv/np.sqrt(T), l , m)
+        bullReturnSum += sum(bullReturns)
+    returns.append(bullReturnSum/count)
+    
+    sim = pd.DataFrame({'returns':returns})
+
+    filepath = Path('/Users/daryakaviani/bullSim/sim.csv') 
+    filepath.parent.mkdir(parents=True, exist_ok=True)
+    sim.to_csv(filepath) 
+
+def simBullLiteSigmasAverageHistorical(count):
+    lm = generateLM()
+    results = pd.DataFrame()
+    for i, row in lm.iterrows():
+        results[(row['l'], row['m'])] = np.nan
+
+    returns = []
+    liqs = []
+    ls = []
+    ms = []
+    for (colname, _) in results.iteritems():
+        l, m = colname
+        print(colname)
+        bullReturnSum = 0
+        CRSum = np.zeros(T)
+        for i in range(count):
+            bullReturn, CRs = simBullLite(eth_rv/np.sqrt(T), l, m, 2)
+            bullReturnSum += sum(bullReturn)
+            CRSum = list(map(add, CRSum, CRs))
+        CRAvg = [c/count for c in CRSum]
+        liquidations = np.sum(i <= 1.5 for i in CRAvg)
+        returns.append(bullReturnSum/count)
+        liqs.append(liquidations)
+        ls.append(l)
+        ms.append(m)
+    
+    sim = pd.DataFrame({'returns':returns,
+                        'l':ls,
+                        'm':ms,
+                        'liquidations': liqs,
+                        'CR': [getCR(ls[i], ms[i], eth_rv) for i in range(len(ls))],
+                        'delta': [getDelta(ls[i], ms[i]) for i in range(len(ls))]})
+
+    filepath = Path('/Users/daryakaviani/bullSim/sim.csv') 
+    filepath.parent.mkdir(parents=True, exist_ok=True)
+    sim.to_csv(filepath) 
+
 def generateLM():
     lm = pd.DataFrame()
     lm['l'] = np.nan
     lm['m'] = np.nan
     lm['delta'] = np.nan
     lm['CR'] = np.nan
-    for l in np.arange(0.01, 1.1, 0.01):
-        for m in np.arange(1, 11, .05):
+    for l in np.arange(0.2, 1.1, 0.1):
+        for m in np.arange(0, 5, 0.1):
             delta = getDelta(l, m)
-            cr = getCR(l, m, 0.8)
-            if cr >= minCR and cr <= maxCR and delta >= 0.55 and delta <= 1:
+            cr = getCR(l, m, eth_rv)
+            print(cr, delta)
+            if cr >= 1.75 and cr <= 3 and delta >= 0.55 and delta <= 1:
                 row = {'l': l, 'm': m, 'delta': delta, 'CR': cr}
                 lm = lm.append(row, ignore_index = True)
     filepath = Path('/Users/daryakaviani/bullSim/lm.csv') 
@@ -174,6 +287,11 @@ def generateLM():
 # print(getDelta(.618034, .48541))
 # print(getCR(.618034, .689808, .8))
 # print(getDelta(.618034, .689808))
-print(simBullLiteSigmasAverage(10000, [sigma/np.sqrt(365) for sigma in np.arange(0.4, 1.3, 0.1)]))
+# print(simBullLiteSigmasAverage(1000))
+print(simBullLiteSigmasAverageHistorical(100))
+# print(getDelta(1, 0))
+# print(getCR(1, 0, 0.89))
+# print(simBullLiteSigmasTrueHistorical(100000))
+
 # %%
 # generateLM()
